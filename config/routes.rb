10995efb8eb6bc5d2e821df2b354702718fa9153
artex <htettunlu15898@gmail.com>
2021-09-27 12:05:22 +0630

    second_commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :posts
   post "api/applicant/confirm", to: "applicant#confirm"
   get "api/applicant", to: "applicant#index"
-  get "/api/applicant/detail", to: "applicant#details"
-  delete "/api/applicant/destroy", to: "applicant#destroy"
+  get "/detail", to: "applicant#details"
+  delete "/destroy", to: "applicant#destroy"
   post "/api/applicant/create", to: "applicant#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
